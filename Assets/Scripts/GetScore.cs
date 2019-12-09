9e01cd62f07df9d509cf55bd21e492f603e4deba
dgutsav <utsav@hitwicket.com>
2019-12-09 15:17:07 +0530

    Second Commit

--- Assets/Scripts/GetScore.cs
+++ Assets/Scripts/GetScore.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text;
 
 public class GetScore : MonoBehaviour
 {
     private Manager manager;
     public Text scoreText;
+    private string[] statements = {"Game Over", "Disappointing","Better Luck Next Time"};
+
+    public bool isOver;
     void Start(){
         manager = FindObjectOfType<Manager>();
+        
     }
     void Update(){
         string sc = manager.getScore().ToString();
         //Debug.Log(sc);
         //scoreText.text = "OBSTACLES DODGED: "+sc;
         scoreText.text = "OBSTACLES DODGED: "+sc;
+        if(manager.isOver() && isOver==false){
+            scoreText.text = statements[(int)(Random.Range(0,3))];
+            isOver = true;
+
+        }
     }
 }
